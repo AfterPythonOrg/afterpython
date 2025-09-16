9e9e2ed7038d86439c56298b8d5dee68e9518143
stephenyau <softwareentrepreneer@gmail.com>
2025-09-16 17:27:37 +0800

    _

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -6,15 +6,17 @@ const outputDir = "../afterpython/_build";
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter({
-		// default options are shown. On some platforms
-		// these options are set automatically — see below
-		pages: outputDir,
-		assets: outputDir,
-		fallback: '200.html', // may differ from host to host
-		precompress: false,
-		strict: true
-	}) },
+	kit: { 
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: outputDir,
+			assets: outputDir,
+			fallback: '200.html', // may differ from host to host
+			precompress: false,
+			strict: true,
+		}) 
+	},
 	extensions: ['.svelte', '.svx']
 };
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,38 +2,45 @@ import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
-const isDev = process.env.NODE_ENV === "development";
-
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	build: {
-		minify: isDev ? false : "esbuild",
-		sourcemap: isDev,
-	},
-	test: {
-		workspace: [
-			{
-				extends: './vite.config.ts',
-				plugins: [svelteTesting()],
-				test: {
-					name: 'client',
-					environment: 'jsdom',
-					clearMocks: true,
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
+export default defineConfig(({ mode }) => {
+	const isDev = mode === "development";
+	return {
+		plugins: [tailwindcss(), sveltekit()],
+    resolve: {
+			alias: {
+				'@': path.resolve('./src')
 			}
-		]
-	}
+		},
+		build: {
+			minify: isDev ? false : "esbuild",
+			sourcemap: isDev,
+		},
+		test: {
+			workspace: [
+				{
+					extends: './vite.config.ts',
+					plugins: [svelteTesting()],
+					test: {
+						name: 'client',
+						environment: 'jsdom',
+						clearMocks: true,
+						include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+						exclude: ['src/lib/server/**'],
+						setupFiles: ['./vitest-setup-client.ts']
+					}
+				},
+				{
+					extends: './vite.config.ts',
+					test: {
+						name: 'server',
+						environment: 'node',
+						include: ['src/**/*.{test,spec}.{js,ts}'],
+						exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+					}
+				}
+			]
+		}
+	};
 });
