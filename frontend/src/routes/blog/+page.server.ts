8dbfccf0c99e82e499a2a8d5347a35512805630d
softwareentrepreneer <softwareentrepreneer@gmail.com>
2025-11-03 19:16:34 -0500

    separate the frontend part from the cli

--- frontend/src/routes/blog/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-import blogPosts from '$static/blog/blog_posts.json';
-
-export const load: PageServerLoad = () => {
-  return {
-    blogPosts,
-  };
-};
